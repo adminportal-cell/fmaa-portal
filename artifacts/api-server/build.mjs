5ccc6bdfddc759ad63170efca885cbef32f9c80f
adminportal-cell <adminportal@fmaa.com.au>
2026-08-21 11:56:04 +1000

    Update build.mjs

--- artifacts/api-server/build.mjs
+++ artifacts/api-server/build.mjs
@@ -15,7 +15,7 @@ async function buildAll() {
   await rm(distDir, { recursive: true, force: true });
 
   await esbuild({
-     entryPoints: [ path.resolve(artifactDir, "src/index.ts"), path.resolve(artifactDir, "src/serverless.ts"), ]
+     entryPoints: [ path.resolve(artifactDir, "src/index.ts"), path.resolve(artifactDir, "src/serverless.ts"), ],
     platform: "node",
     bundle: true,
     format: "esm",
